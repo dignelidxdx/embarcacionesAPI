e8c65a09ceede8d90e81bb4e4c5bb91c38dcba52
Your Name <digneli14@gmail.com>
2020-08-26 00:32:19 +0200

    proyecto para el challenge de embarcaciones para ADA

---
+++ src/main/java/ar/com/noaa/api/embarcaciones/EmbarcacionesApplication.java
@@ -0,0 +1,13 @@
+package ar.com.noaa.api.embarcaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarcacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbarcacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/noaa/api/embarcaciones/EmbarcacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.noaa.api.embarcaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarcacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
